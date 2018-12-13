18e9cf1dbfdf630d965280dd4346c7cf1e0ab780
Mayank <Mayank@DESKTOP-E7C5CM6>
2018-12-14 00:43:42 +0530

    Spring boot starter first commit

---
+++ mycarrental/src/main/java/com/mj/org/MycarrentalApplication.java
@@ -0,0 +1,14 @@
+package com.mj.org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycarrentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycarrentalApplication.class, args);
+	}
+
+}
+
