a502592c5932bd24656a34de32d98e3c521742f0
mayank <mayankjha.mj@gmail.com>
2019-01-03 23:40:07 +0530

    Git Intellij Test

--- mycarrental/src/main/java/com/mj/org/MycarrentalApplication.java
+++ mycarrental/src/main/java/com/mj/org/MycarrentalApplication.java
@@ -3,6 +3,8 @@ package com.mj.org;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//adding some comment to test the git using intellij
+
 @SpringBootApplication
 public class MycarrentalApplication {
 
